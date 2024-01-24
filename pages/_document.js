0f8870c9f58341d54dd443010aaf7b928f345c18
elitesuper <tonylobbin7712@gmail.com>
2024-01-24 16:53:35 +0300

    update

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta
           name="description"
-          content="Jano - Creative Multipurpose React NextJS Template"
+          content="Full Stack Developer"
         />
         <meta
           name="keywords"
